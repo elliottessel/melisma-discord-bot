27ba2680a95013372a21b5e8d7fd429dfe94fb52
elliottessel <emtessel430@gmail.com>
2026-05-16 20:43:29 -0700

    outgoing command first draft

--- commands/outgoing.js
+++ commands/outgoing.js
@@ -8,6 +8,8 @@ module.exports = {
     .setDescription('Shows reviews that are overdue'),
 
   async execute(interaction) {
+    await interaction.deferReply();
+
     const shows = await getShows();
 
     const date = new Date();
@@ -16,24 +18,33 @@ module.exports = {
     const outgoing = shows.filter(show => {
         const showDate = new Date(`${show.date}, 2026`); // get show date
 
+        const attended = show.attended === 'TRUE'; // check if attended
+        console.log(show.attended);
+
         const missing = show.reviewWritten !== 'TRUE'; // check if review is missing
 
         const passed = showDate < date; // check if date is passed
 
-        return missing && passed; // only return shows that are missing and past date
+        return attended && missing && passed; // only return shows that are missing and past date
     });
 
     if (outgoing.length === 0) {
-        return interaction.reply(
+        return interaction.editReply(
             'No outgoing reviews right now.'
         );
     }
 
-    const message = outgoing.map(show =>
-        `• ${show.artist} — ${show.reporter || 'Unassigned'} — ${show.date}`
-        ).join('\n');
+    const limited = outgoing.slice(0, 5);
 
-    await interaction.reply({
+    const message = limited.map(show =>
+
+        `• ${show.artist}
+        Reporter: ${show.reporter || 'Unassigned'}
+        Date: ${show.date}`
+
+    ).join('\n\n');
+
+    await interaction.editReply({
         content: `Outgoing Reviews:\n\n${message}`,
     });
   },
